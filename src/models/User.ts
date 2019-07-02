1b77a00016c38c6e7a84d48e4a94a34e6e4dc2b3
Lionel Adams <lionel.adams@diconium.com>
2019-07-02 13:00:28 +0100

    feat: Add Eventing instance into User class

--- src/models/User.ts
+++ src/models/User.ts
@@ -1,4 +1,4 @@
-import axios, { AxiosResponse } from 'axios';
+import { Eventing } from './Eventing';
 
 interface UserProps {
   id?: number;
@@ -6,15 +6,8 @@ interface UserProps {
   age?: number;
 }
 
-type Callback = () => void;
-
-const SERVER_API: string = 'http://localhost:3000';
-const ENDPOINT = {
-  USERS: 'users',
-};
-
 export class User {
-  events: { [key: string]: Callback[] } = {};
+  events: Eventing = new Eventing();
 
   constructor(private data: UserProps) {}
 
@@ -36,61 +29,5 @@ export class User {
    */
   set(update: UserProps): void {
     Object.assign(this.data, update);
-  }
-
-  /**
-   * Register an event with its callback function
-   * @method on
-   * @param {String} eventName
-   * @param {Callback} callback
-   */
-  on(eventName: string, callback: Callback): void {
-    const handlers = this.events[eventName] || [];
-
-    handlers.push(callback);
-
-    this.events[eventName] = handlers;
-  }
-
-  /**
-   * Trigger the eventName invoked
-   * @method trigger
-   * @param {string} eventName
-   * @return void
-   */
-  trigger(eventName: string): void {
-    const handlers = this.events[eventName];
-
-    if (!handlers || handlers.length === 0) {
-      return; // no eventname found, get outa here...
-    }
-
-    handlers.forEach(callback => callback());
-  }
-
-  // TODO: User::fetch - return Promise object
-  fetch(): void {
-    axios.get(`${SERVER_API}/${ENDPOINT.USERS}/${this.get('id')}`).then(
-      (response: AxiosResponse): void => {
-        console.log('User is', response.data);
-
-        this.set(response.data);
-      },
-    );
-  }
-
-  save(): void {
-    const id = this.get('id');
-
-    if (id) {
-      console.log('save with id');
-      // put
-      // axios.put(`http://localhost:3000/users/${id}`, this.data);
-      axios.put(`${SERVER_API}/${ENDPOINT.USERS}/${id}`, this.data);
-    } else {
-      //post
-      console.log('save new record');
-      axios.post(`${SERVER_API}/${ENDPOINT.USERS}`, this.data);
-    }
   }
 }
